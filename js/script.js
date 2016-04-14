4b375aaaaadeca82e889fd0aa09bd98830ec9df3
miharyan <miharyan@129-79-116-214.dhcp-bl.indiana.edu>
2016-04-14 11:13:25 -0400

    new query

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-$('button').click(function() {console.log("Clicked: ", this);
+$(document).ready(function () {
+
+	$('button').click(function() {
+		console.log("Clicked: ", this);
+		$(this).addClass("chosen");
+	});
+$( "#Kale" ).append( $( "p:first" ).hasClass( "chosen" ).toString() )
+$( "#Chinese Broccoli" ).append( $( "p:first" ).hasClass( "chosen" ).toString() )	
+		
+});
+
