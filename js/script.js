c43464407b879be9f5b88009e2474a8d0651308a
miharyan <miharyan@imail.iu.edu>
2016-04-19 23:57:31 -0400

    editted html

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 		console.log("Clicked: ", this);
 		$(this).addClass("chosen");
 		var answer= $(this).hasClass("correct");
-		if (answer == true) { 
+		if (answer == true) {
 		console.log("It's Correct!");
 		$(this).addClass("right");
 		} else {
@@ -12,36 +12,7 @@ $(document).ready(function () {
 		$(this).addClass("wrong");
 		}
 		$(this).siblings('button').prop("disabled", true);
-		
 	});
-	
-	$('submit').click(function(){
-		function gradeTest() {
-	var totalQuestions = 5;
-	var correctAnswers = 0;
-	var alertText;
-	var i;
-	
-var answer = document.getElementByName(".button");
-	for(i = 0; i < answer.length; i++) {
-		if(answer[i].checked)
-			if(answer[i].hasClass("correct") {
-				correctAnswers++;
-				break;
-			}
-		}
-	}
-	
-	if(correctAnswers == totalQuestions) {
-		alertText = "Congratulations! You got all the questions right!";
-}
-	else {
-		alertText = "You got " + correctAnswers + " out of " + totalQuestions + " correct!";
-}
-	alert(alertText);
-}	
-	});
-		
+
+
 });
-
-
