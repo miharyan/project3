0f6a84c93e75b2b1380caf1d78bb23c1866e7b06
miharyan <miharyan@129-79-116-214.dhcp-bl.indiana.edu>
2016-04-19 21:59:41 -0400

    add jquery sign in front of the function gradeTest()

--- js/script.js
+++ js/script.js
@@ -14,68 +14,34 @@ $(document).ready(function () {
 		$(this).siblings('button').prop("disabled", true);
 		
 	});
-
-		
-});
-
-function gradeTest() {
+	
+	$('submit').click(function(){
+		function gradeTest() {
 	var totalQuestions = 5;
 	var correctAnswers = 0;
 	var alertText;
 	var i;
 	
-var a1 = document.getElementByName('button');
-	for(i = 0; i < a1.length; i++) {
-		if(a1[i].checked)
-			if(a1[i].hasClass("correct") {
+var answer = document.getElementByName(".button");
+	for(i = 0; i < answer.length; i++) {
+		if(answer[i].checked)
+			if(answer[i].hasClass("correct") {
 				correctAnswers++;
 				break;
 			}
 		}
 	}
-var a2 = document.getElementByName('button');
-	for(i = 0; i < a2.length; i++) {
-		if(a2[i].checked)
-			if(a2[i].hasClass("correct") {
-				correctAnswers++;
-				break;
-			}
-		}
-	}
-var a3 = document.getElementByName('button');
-	for(i = 0; i < a3.length; i++) {
-		if(a3[i].checked)
-			if(a3[i].hasClass("correct") {
-				correctAnswers++;
-				break;
-			}
-		}
-	}
-var a4 = document.getElementByName('button');
-	for(i = 0; i < a4.length; i++) {
-		if(a4[i].checked)
-			if(a4[i].hasClass("correct") {
-				correctAnswers++;
-				break;
-			}
-		}
-	}
-var a5 = document.getElementByName('button');
-	for(i = 0; i < a5.length; i++) {
-		if(a5[i].checked)
-			if(a5[i].hasClass("correct") {
-				correctAnswers++;
-				break;
-			}
-		}
-	}
-
 	
-if(correctAnswers == totalQuestions) {
-	alertText = "Congratulations! You got all the questions right!";
+	if(correctAnswers == totalQuestions) {
+		alertText = "Congratulations! You got all the questions right!";
 }
-else {
-	alertText = "You got " + correctAnswers + " out of " + totalQuestions + " correct!";
+	else {
+		alertText = "You got " + correctAnswers + " out of " + totalQuestions + " correct!";
 }
-alert(alertText);
+	alert(alertText);
 }	
+	});
+		
+});
+
+
